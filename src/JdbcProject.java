28e9c6d0a8d86aa4235afd7d6d9c9e992afa9c3d
Shubh Tandon <shubhtandon802@gmail.com>
2025-11-18 22:16:42 +0530

    showcasing all data from table

--- src/JdbcProject.java
+++ src/JdbcProject.java
@@ -6,7 +6,7 @@ public class JdbcProject {
         String url = "jdbc:mysql://localhost:3306/JavaDB";
         String uname = "ShubhMySQL";
         String pass = "Shubh@162630";
-        String query = "select * from firstjdbctable where name like 'a%'";
+        String query = "select * from firstjdbctable";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (Exception e) {
@@ -17,8 +17,16 @@ public class JdbcProject {
             Connection con = DriverManager.getConnection(url, uname, pass);
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(query);
-            while(rs.next())
-                System.out.println(rs.getString("name"));
+            while (rs.next()) {
+                String user = "id: " + rs.getInt("id") + ", " +
+                        "name: " + rs.getString("name") + ", " +
+                        "dob: " + rs.getDate("dob") + ", " +
+                        "email: " + rs.getString("email") + ", " +
+                        "phone: " + rs.getString("phone_no") + ", " +
+                        "created_at: " + rs.getTimestamp("created_at") + ", " +
+                        "updated_at: " + rs.getTimestamp("updated_at");
+                System.out.println(user);
+            }
             st.close();
             con.close();
         } catch (SQLException e) {
